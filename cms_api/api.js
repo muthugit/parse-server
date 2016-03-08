b047452c5ac748f7d7035bc127df0f6ba8bf2cf5
Muthupandian <contact@muthupandian.in>
2016-03-09 03:46:29 +0530

    Change

--- cms_api/api.js
+++ cms_api/api.js
@@ -5,6 +5,26 @@ app.get('/', function(req, res) {
 	var Parse = require('parse/node');
 	Parse.initialize("myAppId");
 	Parse.serverURL = 'http://localhost:1337/parse';
+	
+	var GameScore = Parse.Object.extend("GameScore");
+	var gameScore = new GameScore();
+	gameScore.set("score", 1337);
+	gameScore.set("playerName", "Sean Plott");
+	gameScore.set("cheatMode", false);
+
+	gameScore.save(null, {
+		success : function(gameScore) {
+			// Execute any logic that should take place after the object is
+			// saved.
+			res.send('New object created with objectId: ' + gameScore.id);
+		},
+		error : function(gameScore, error) {
+			// Execute any logic that should take place if the save fails.
+			// error is a Parse.Error with an error code and message.
+			console.log('Failed to create new object, with error code: '
+					+ error.message);
+		}
+	});
 
 	var userRepository1 = require("./user.js");
 
@@ -12,6 +32,6 @@ app.get('/', function(req, res) {
 	var output = userRepositoryInstance.addUser(Parse, res);
 });
 
-app.listen(3000, function() {
-	console.log('Example app listening on port 3000!');
+app.listen(6000, function() {
+	console.log('Example app listening on port 6000!');
 });
